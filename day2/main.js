26206f1ecf18701546f1473654276b48ac9087aa
Jukka Pajarinen <jukka.pajarinen@lumitum.com>
2020-12-02 19:18:21 +0200

    Solution for AoC day 2 puzzles.

---
+++ day2/main.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+
+let path = process.cwd();
+let buffer = fs.readFileSync(`${path}/data.txt`);
+let rows = buffer.toString().split("\n").filter(row => row.length > 0);
+
+let validCount = 0;
+let validCount2 = 0;
+rows.forEach(row => {
+  let [policy, password] = row.split(": ");
+  let [policyIntervals, policyChar] = policy.split(" ");
+  let policyFrom = parseInt(policyIntervals.split("-")[0]);
+  let policyTo = parseInt( policyIntervals.split("-")[1]);
+  let occurencies = password.split(policyChar).length - 1;
+
+  if (occurencies >= policyFrom && occurencies <= policyTo) {
+    validCount++;
+  }
+
+  let char1 = password[policyFrom-1];
+  let char2 = password[policyTo-1];
+  
+  if (char1 !== char2 && (char1 === policyChar || char2 === policyChar)) {
+    validCount2++;
+  }
+});
+console.log(`Valid passwords: ${validCount} out of ${rows.length} (part1)`);
+console.log(`Valid passwords: ${validCount2} out of ${rows.length} (part2)`);

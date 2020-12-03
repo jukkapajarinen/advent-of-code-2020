b5908504a5df2cc24d5582587442606638aeee6f
Jukka Pajarinen <jukka.pajarinen@lumitum.com>
2020-12-03 20:27:25 +0200

    Solution for AoC day 3 puzzles.

---
+++ day3/main.js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+
+let path = process.cwd();
+let buffer = fs.readFileSync(`${path}/data.txt`);
+let rows = buffer.toString().split("\n").filter(row => row.length > 0);
+let slope = [];
+const horizontalStretch = 1000;
+
+rows.forEach((row, idx) => {
+  slope[idx] = row.repeat(horizontalStretch).split("");
+});
+
+let treeCounter = (slope, right, down) => {
+  let printSlope = JSON.parse(JSON.stringify(slope));
+  let treeCount = 0;
+  for (let i = 0; i < slope.length; i += down) {
+    if (i == 0) {
+      continue;
+    }
+
+    let position = i / down * right;
+
+    if (slope[i][position] === "#") {
+      treeCount++;
+      printSlope[i][position] = "X";
+    }
+    else {
+      printSlope[i][position] = "O";
+    }
+  }
+
+  return treeCount;
+};
+
+let r1d1 = treeCounter(slope, 1, 1);
+let r3d1 = treeCounter(slope, 3, 1);
+let r5d1 = treeCounter(slope, 5, 1);
+let r7d1 = treeCounter(slope, 7, 1);
+let r1d2 = treeCounter(slope, 1, 2);
+
+console.log(`Trees 1,1: ${r1d1}`);
+console.log(`Trees 3,1: ${r3d1}`);
+console.log(`Trees 5,1: ${r5d1}`);
+console.log(`Trees 7,1: ${r7d1}`);
+console.log(`Trees 1,2: ${r1d2}`);
+console.log(`Tree product: ${r1d1 * r3d1 * r5d1 * r7d1 * r1d2}`);

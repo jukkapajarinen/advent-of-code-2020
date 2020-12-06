bc8dc65694d90ced7c1e6b5ad082bd11be722aeb
Jukka Pajarinen <jukka.pajarinen@lumitum.com>
2020-12-06 13:43:16 +0200

    Cleaning day 1 solution to be much nicer.

--- day-01/main.js
+++ day-01/main.js
@@ -4,30 +4,25 @@ let path = process.argv[1].replace("/main.js", "");
 let buffer = fs.readFileSync(`${path}/input.txt`);
 let rows = buffer.toString().split("\n");
 
-rows.forEach(operand1 => {
-  if (!isNaN(parseInt(operand1))) {
+let sumOf2020 = puzzle => {
+  let product = -1;
+  rows.forEach(operand1 => {
     rows.forEach(operand2 => {
-      if (!isNaN(parseInt(operand2))) {
-        if (parseInt(operand1) + parseInt(operand2) === 2020) {
-          console.log(`Part 1: ${operand1} * ${operand2} = ${parseInt(operand1) * parseInt(operand2)}`);
-        }
+      if (parseInt(operand1) + parseInt(operand2) === 2020) {
+        product = parseInt(operand1) * parseInt(operand2);
       }
-    });
-  }
-});
 
-rows.forEach(operand1 => {
-  if (!isNaN(parseInt(operand1))) {
-    rows.forEach(operand2 => {
-      if (!isNaN(parseInt(operand2))) {
+      if (puzzle === 2) {
         rows.forEach(operand3 => {
-          if (!isNaN(parseInt(operand3))) {
-            if (parseInt(operand1) + parseInt(operand2) + parseInt(operand3) === 2020) {
-              console.log(`Part 2: ${operand1} * ${operand2} * ${operand3} = ${parseInt(operand1) * parseInt(operand2) * parseInt(operand3)}`);
-            }
+          if (parseInt(operand1) + parseInt(operand2) + parseInt(operand3) === 2020) {
+            product = parseInt(operand1) * parseInt(operand2) * parseInt(operand3);
           }
         });
       }
     });
-  }
-});
+  });
+  return product;
+};
+
+console.log(`Product of 2 entries: ${sumOf2020(1)} (part 1)`);
+console.log(`Product of 3 entries: ${sumOf2020(2)} (part 2)`);

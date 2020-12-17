c95bcccb8e6b66aea541b551b159ef852b79df26
Jukka Pajarinen <jukka.pajarinen@lumitum.com>
2020-12-17 21:51:07 +0200

    Valid tickets processing for AoC day 16 part2.

--- day-16/main.js
+++ day-16/main.js
@@ -18,20 +18,45 @@ rules = rules.split("\n").map(r => {
 myTicket = myTicket.split("\n").slice(1)[0].split(",").map(v => parseInt(v));
 tickets = tickets.split("\n").slice(1).map(t => t.split(",").map(v => parseInt(v)));
 
-let invalidSum = 0;
-tickets.forEach(ticket => {
-  let inRanges = ticket.reduce((a, b) => ({...a, [b]: false}), ticket[0]);
-  ticket.forEach(number => {
-    rules.forEach(rule => {
-      let inRange = (number >= rule.range1[0] && number <= rule.range1[1] || 
-        number >= rule.range2[0] && number <= rule.range2[1]);
-      if (inRange) {
-        inRanges[number] = true;
-      }
+let ticketErrorRate = (rules, tickets) => {
+  let invalidSum = 0;
+  tickets.forEach(ticket => {
+    let inRanges = ticket.reduce((a, b) => ({...a, [b]: false}), ticket[0]);
+    ticket.forEach(number => {
+      rules.forEach(rule => {
+        let inRange = (number >= rule.range1[0] && number <= rule.range1[1] || 
+          number >= rule.range2[0] && number <= rule.range2[1]);
+        if (inRange) {
+          inRanges[number] = true;
+        }
+      });
     });
+    invalidSum += Object.entries(inRanges).reduce((a, b) => a + (!b[1] ? parseInt(b[0]) : 0), 0);
   });
-  invalidSum += Object.entries(inRanges).reduce((a, b) => a + (!b[1] ? parseInt(b[0]) : 0), 0);
-});
+  return invalidSum;
+}
 
-console.log(`Ticket scanning error rate: ${invalidSum} (part 1)`);
+let validTickets = (rules, tickets) => {
+  let valids = [], current = 0;
+  tickets.forEach(ticket => {
+    let inRanges = ticket.reduce((a, b) => ({...a, [b]: false}), ticket[0]);
+    ticket.forEach(number => {
+      rules.forEach(rule => {
+        let inRange = (number >= rule.range1[0] && number <= rule.range1[1] || 
+          number >= rule.range2[0] && number <= rule.range2[1]);
+        if (inRange) {
+          inRanges[number] = true;
+        }
+      });
+    });
+    if (!Object.values(inRanges).includes(false)) {
+      valids.push(tickets[current]);
+    }
+    current++;
+  });
+  return valids;
+};
+
+let allTickets = [myTicket, ...validTickets(rules, tickets)];
+console.log(`Ticket scanning error rate: ${ticketErrorRate(rules, tickets)} (part 1)`);
 // console.log(`Another something: ${2} (part 2)`);

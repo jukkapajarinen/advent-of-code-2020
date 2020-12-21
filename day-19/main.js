70c38cc1258ca8b5a94e1ec8d7fa8340c0e9b397
Jukka Pajarinen <jukka.pajarinen@lumitum.com>
2020-12-22 00:43:42 +0200

    Solution for AoC day 19 part 1.

--- day-19/main.js
+++ day-19/main.js
@@ -3,19 +3,36 @@ const fs = require("fs");
 
 const path = process.argv[1].replace("/main.js", "");
 const buffer = fs.readFileSync(`${path}/input.txt`);
-const [ruleStrings, messages] = buffer.toString().trimEnd().split("\n\n").map(x => x.split("\n"));
+const [rules, messages] = buffer.toString().trimEnd().split("\n\n").map(a => a.split("\n"));
 
-let rules = {};
-ruleStrings.forEach(rule => {
-  let [id, val] = rule.split(": ");
-  if (val.includes("\"")) { rules[id] = val.replace(/"/g, ""); }
-  else if (val.includes("|")) { rules[id] = val.split(" | ").map(v => v.split(" ")); }
-  else { rules[id] = val.split(" "); }
-});
+let parsed = rules => {
+  let parsed = new Map();
+  rules.forEach(rule => {
+    let [id, val] = rule.split(": ");
+    if (val.includes("\"")) { 
+      parsed.set(parseInt(id), val.replace(/"/g, ""));
+    }
+    else if (val.includes("|")) { 
+      parsed.set(parseInt(id), val.split(" | ").map(a => a.split(" ")
+        .map(b => parseInt(b))));
+    }
+    else { 
+      parsed.set(parseInt(id), [val.split(" ").map(a => parseInt(a))]);
+    }
+  });
+  return parsed;
+};
 
-console.log(rules);
-console.log(messages);
+let generateRegex = (ruleId, rules) => {
+  const recursiveRegex = id => {
+    let rule = rules.get(id);
+    return ['a', 'b'].includes(rule) ? rule 
+      : `(${rule.map(a => a.map(b => recursiveRegex(b, rules)).join("")).join("|")})`;
+  };
+  return new RegExp(`^${recursiveRegex(0)}$`);
+};
 
-let matchingMessages = -1;
-console.log(`Matching rules for rule 0: ${matchingMessages} (part 1)`);
+let regex = generateRegex(0, parsed(rules));
+let matches = messages.filter(msg => msg.match(regex)).length;
+console.log(`Matching rules: ${matches} from ${messages.length} (part 1)`);
 // console.log(`Another something: ${2} (part 2)`);
